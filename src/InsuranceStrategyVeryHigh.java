c9427fee182c8d1e98291104a0375b5ce1cc63ee
wpenglish <wpenglish27@gmail.com>
2021-01-25 12:10:17 -0500

    Fine tuning

--- src/InsuranceStrategyVeryHigh.java
+++ src/InsuranceStrategyVeryHigh.java
@@ -1,19 +1,18 @@
-public class InsuranceStrategyVeryHigh {
+public class InsuranceStrategyVeryHigh extends InsuranceStrategy {
     public InsuranceStrategyVeryHigh() {
     }
 
-    public double calculateInsuranceVeryHigh(double income) {
-        return (income - getAdjustment()) * getWeight() + getConstant();
-    }
-
+    @Override
     public int getConstant() {
         return 105600;
     }
 
+    @Override
     public double getWeight() {
         return 0.02;
     }
 
+    @Override
     public int getAdjustment() {
         return 60000;
     }
